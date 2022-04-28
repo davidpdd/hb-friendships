486b587b262698a5c3bacecd3fff66fe15647e58
dpietrzykowski <test@test.com>
2022-04-28 10:00:19 +0100

    Add layout style to BoatList Aura component

--- force-app/main/default/aura/BoatList/BoatListController.js
+++ force-app/main/default/aura/BoatList/BoatListController.js
@@ -1,30 +1,30 @@
 ({
-    doInit : function(component, event, helper) {
-        console.log('init called');
-        const action = component.get("c.getAllBoats");
-        action.setCallback(this, function(response){
-            // verifie si tout s'est bien passe
-            const state = response.getState();
-            
-            console.log(state);
-            
-            // Si ok
-            if(state === "SUCCESS"){
-                // recupere la valeur retournee
-                const boats = response.getReturnValue(); 
-                
-                // faire passer cette valeur retournee au front
-                component.set("v.boats", boats);
-                
-                console.log(JSON.stringify(boats));
-               
-            } else {
-               // gerer l'erreur
-            }
-        });
-        
-        // SUPER IMPORTANT
-        $A.enqueueAction(action);
-       
-    }
+  doInit: function (component, event, helper) {
+    console.log('init called')
+    const action = component.get("c.getAllBoats")
+    action.setCallback(this, function (response) {
+      // verifie si tout s'est bien passe
+      const state = response.getState()
+
+      console.log(state)
+
+      // Si ok
+      if (state === "SUCCESS") {
+        // recupere la valeur retournee
+        const boats = response.getReturnValue()
+
+        // faire passer cette valeur retournee au front
+        component.set("v.boats", boats)
+
+        console.log(JSON.stringify(boats))
+
+      } else {
+        // gerer l'erreur
+      }
+    })
+
+    // SUPER IMPORTANT
+    $A.enqueueAction(action)
+
+  },
 })
