f2f2ac856b27e9fe40b21b9bdb29ea277416653a
dpietrzykowski <test@test.com>
2022-05-05 09:39:50 +0100

    Add search and iteration to boat search lwc component

--- force-app/main/default/lwc/boatSearchByName/boatSearchByName.js
+++ force-app/main/default/lwc/boatSearchByName/boatSearchByName.js
@@ -1,18 +1,30 @@
-import {LightningElement, wire} from 'lwc'
-import getAvailableBoats from "@salesforce/apex/BoatSearchController.getAvailableBoats"
+import { LightningElement, wire } from "lwc";
+import getAvailableBoats from "@salesforce/apex/BoatSearchController.getAvailableBoats";
 
 export default class BoatSearchByName extends LightningElement {
-
-  availableBoats = []
+  availableBoats = [];
+  filteredBoats = [];
 
   @wire(getAvailableBoats)
-  wiredBoats({data, error}){
-    if(data){
+  wiredBoats({ data, error }) {
+    if (data) {
       this.availableBoats = data;
-      console.log(` ${JSON.stringify(this.availableBoats, null, 2)}`) // TODO: delete
+      this.filteredBoats = this.availableBoats;
+      console.table(data);
+      console.log(` ${JSON.stringify(this.availableBoats, null, 2)}`); // TODO: delete
     } else {
       console.error(error);
     }
   }
 
+  handleSearch(event) {
+    const name = event.detail.value;
+    if (name === "") {
+      this.filteredBoats = this.availableBoats;
+    } else {
+      this.filteredBoats = this.availableBoats.filter((boat) =>
+        boat.Name.toLowerCase().includes(name.toLowerCase())
+      );
+    }
+  }
 }
